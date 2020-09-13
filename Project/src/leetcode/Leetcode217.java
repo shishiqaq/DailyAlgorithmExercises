fcb622286862f01325f91637381e3592c26b72a8
panda <69600303+shishiqaq@users.noreply.github.com>
2020-09-13 12:29:29 +0800

    Create Leetcode217.java

---
+++ Project/src/leetcode/Leetcode217.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Leetcode217 {
+	public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int num : nums){
+            if(set.contains(num))
+                return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
