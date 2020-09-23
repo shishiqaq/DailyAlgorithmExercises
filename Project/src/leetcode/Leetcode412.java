de6e8dc98514ed6722e0dff56c304ad5b8b6dd58
panda <69600303+shishiqaq@users.noreply.github.com>
2020-09-24 01:42:31 +0800

    Create Leetcode412.java

---
+++ Project/src/leetcode/Leetcode412.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Leetcode412 {
+	public List<String> fizzBuzz(int n) {
+        List<String> list = new LinkedList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 != 0 && i % 5 != 0)
+                list.add(Integer.toString(i));
+            if(i % 15 == 0)
+                list.add("FizzBuzz");
+            else if(i % 3 == 0)
+                list.add("Fizz");
+            else if(i % 5 == 0)
+                list.add("Buzz");
+        }
+        return list;
+    }
+}
