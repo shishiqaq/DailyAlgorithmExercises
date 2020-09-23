b3d1f441ea530d9fcef78ad11fa32590df84f7e2
panda <69600303+shishiqaq@users.noreply.github.com>
2020-09-24 01:06:10 +0800

    Create Leetcode172.java

---
+++ Project/src/leetcode/Leetcode172.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Leetcode172 {
+	public int trailingZeroes(int n) {
+        int count = 0;
+        while(n >= 5){
+            count += n/5;
+            n /= 5;
+        }
+        return count;
+    }
+}
