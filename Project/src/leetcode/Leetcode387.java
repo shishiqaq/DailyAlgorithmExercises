114d64445f183abfcb07419d019fb4a8739b8de9
panda <69600303+shishiqaq@users.noreply.github.com>
2020-09-24 01:22:29 +0800

    Update Leetcode387.java

--- Project/src/leetcode/Leetcode387.java
+++ Project/src/leetcode/Leetcode387.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 public class Leetcode387 {
+	//O(n^2)
 	public int firstUniqChar(String s) {
         for(int i = 0; i < s.length(); i++){
             if(isUnique(s, s.charAt(i)))
@@ -16,7 +17,21 @@ public class Leetcode387 {
             if(count > 2)
             return false;
         }
-        
+      
         return true;
+    }
+    //O(n)
+    public int firstUniqChar2(String s) {
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            int index = s.charAt(i)-97;
+            count[index]++;
+        }
+            
+        for(int i = 0; i < s.length(); i++){
+            if(count[s.charAt(i)-97] == 1)
+                return i;
+        }
+        return -1;
     }
 }
