ab0e48eec3169d121659e5eacf8e564b3fbca1fc
panda <69600303+shishiqaq@users.noreply.github.com>
2020-10-13 14:58:46 +0800

    Create Leetcode104.java

---
+++ Project/src/leetcode/Leetcode104.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Leetcode104 {
+	
+	 public class TreeNode {
+		 int val;
+		 TreeNode left;
+	     TreeNode right;
+	     TreeNode() {}
+	     TreeNode(int val) { this.val = val; }
+	     TreeNode(int val, TreeNode left, TreeNode right) {
+	    	 this.val = val;
+	    	 this.left = left;
+	    	 this.right = right;
+		 }
+	 }
+	 
+	public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
