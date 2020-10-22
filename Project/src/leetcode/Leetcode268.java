6e26fe7e195ffbbc75091e18c629caa1221ce999
panda <69600303+shishiqaq@users.noreply.github.com>
2020-10-22 23:06:49 +0800

    Create Leetcode268.java

---
+++ Project/src/leetcode/Leetcode268.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Leetcode268 {
+	public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
