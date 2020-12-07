e3dbf123bb2fe2d22e0d4078bd560bf620c72dae
panda <69600303+shishiqaq@users.noreply.github.com>
2020-12-07 23:59:36 +0800

    Create Leetcode120.java

---
+++ Project/src/leetcode/Leetcode120.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Leetcode120 {
+	public int minimumTotal(List<List<Integer>> triangle) {
+        Integer[][] dp = new Integer[triangle.size()][triangle.size()];
+        return help(triangle, 0, 0, dp);
+    }
+    
+    public int help(List<List<Integer>> tri, int row, int column, Integer[][] dp) {
+        if(row == dp.length-1)
+            return tri.get(row).get(column);
+        if(dp[row][column] != null)
+            return dp[row][column];
+        int left = help(tri, row+1, column, dp);
+        int right = help(tri, row+1, column+1, dp);
+        dp[row][column] = Math.min(left, right) + tri.get(row).get(column);
+        return dp[row][column];
+    }
+}
